c66e93213cb7969f9c618a746ee33ee8e7bee0e2
Tofutych <martirosov.davidka@mail.ru>
2022-11-09 11:04:36 +0300

    govno

---
+++ lab1.py
@@ -0,0 +1,94 @@
+import tkinter
+from tkinter import *
+from tkinter import scrolledtext
+
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
+import time
+
+from Gradient import makeData, Funct_consider
+
+
+def Lab_1_window():
+    def draw():
+        fig.clf()
+
+        ax = fig.add_subplot(projection="3d")
+        ax.plot_surface(
+            x, y, z, rstride=5, cstride=5, alpha=0.8, cmap="twilight_shifted"
+        )
+        canvas.draw()
+
+        res_x = txt_1.get()
+        res_y = txt_2.get()
+        res_step = txt_3.get()
+        res_iterations = txt_4.get()
+
+        x_cs, y_cs, z_cs = Funct_consider(
+            float(res_x), float(res_y), float(res_step), int(res_iterations)
+        )
+
+        for i in range(len(x_cs)):
+            if i < (len(x_cs) - 1):
+                ax.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="red", s=1)
+            else:
+                ax.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="black")
+
+            canvas.draw()
+            print(
+                f"#{i}\tx = {round(x_cs[i], 2)}, y = {round(y_cs[i], 2)}, z = {z_cs[i]}"
+            )
+
+            window_lab_1.update()
+
+            delay = 0
+
+            time.sleep(float(delay))
+
+    window_lab_1 = tkinter.Tk()
+    window_lab_1.wm_title("Gradient Descent Method")
+    window_lab_1.geometry("780x600")
+    window_lab_1.resizable(False, False)
+
+    x, y, z = makeData()
+
+    fig = plt.figure()
+    fig.add_subplot(projection="3d")
+
+    canvas = FigureCanvasTkAgg(fig, master=window_lab_1)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+
+    toolbar = NavigationToolbar2Tk(canvas, window_lab_1)
+    toolbar.update()
+    canvas.get_tk_widget().pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+
+    lbl_1 = Label(window_lab_1, text="X")
+    lbl_1.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    txt_1 = Entry(window_lab_1, width=10)
+    txt_1.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    lbl_2 = Label(window_lab_1, text="Y")
+    lbl_2.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    txt_2 = Entry(window_lab_1, width=10)
+    txt_2.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    lbl_3 = Label(window_lab_1, text="Initial step")
+    lbl_3.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    txt_3 = Entry(window_lab_1, width=10)
+    txt_3.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    lbl_4 = Label(window_lab_1, text="Iterations")
+    lbl_4.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    txt_4 = Entry(window_lab_1, width=10)
+    txt_4.pack(side=TOP, padx=1, pady=1, anchor=NW)
+
+    btn = Button(window_lab_1, text="Выполнить", width=20, command=draw)
+    btn.pack(side=TOP, padx=5, pady=5, anchor=NW)
+
+    tkinter.mainloop()
---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from lab1 import Lab_1_window
+
+
+def main():
+    window = Tk()
+    window.title("Select")
+    window.geometry("90x400")
+    btn_lab1 = Button(window, text="1", command=Lab_1_window, height=1, width=5)
+    btn_lab2 = Button(window, text="2", command=Lab_1_window, height=1, width=5)
+    btn_lab3 = Button(window, text="3", command=Lab_1_window, height=1, width=5)
+    btn_lab4 = Button(window, text="4", command=Lab_1_window, height=1, width=5)
+    btn_lab5 = Button(window, text="5", command=Lab_1_window, height=1, width=5)
+
+    btn_lab1.grid(row=1, column=0, padx=5, pady=5)
+    btn_lab2.grid(row=2, column=0, padx=5, pady=5)
+    btn_lab3.grid(row=3, column=0, padx=5, pady=5)
+    btn_lab4.grid(row=4, column=0, padx=5, pady=5)
+    btn_lab5.grid(row=5, column=0, padx=5, pady=5)
+    window.resizable(False, False)
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
