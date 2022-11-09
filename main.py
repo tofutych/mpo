e042fbcf6cbeeae90b31856e663fef0d2b3756a9
Tofutych <martirosov.davidka@mail.ru>
2022-11-09 11:40:29 +0300

    some cosmetics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-from lab1 import Lab_1_window
+from labs import Lab_1_window
 
 
 def main():
@@ -12,12 +12,15 @@ def main():
     btn_lab3 = Button(window, text="3", command=Lab_1_window, height=1, width=5)
     btn_lab4 = Button(window, text="4", command=Lab_1_window, height=1, width=5)
     btn_lab5 = Button(window, text="5", command=Lab_1_window, height=1, width=5)
+    exit_button = Button(window, text="Exit", command=window.destroy)
 
     btn_lab1.grid(row=1, column=0, padx=5, pady=5)
     btn_lab2.grid(row=2, column=0, padx=5, pady=5)
     btn_lab3.grid(row=3, column=0, padx=5, pady=5)
     btn_lab4.grid(row=4, column=0, padx=5, pady=5)
     btn_lab5.grid(row=5, column=0, padx=5, pady=5)
+    exit_button.grid(row=10, column=0, padx=10, pady=5)
+    exit_button.place(x=15, y=370)
     window.resizable(False, False)
     window.mainloop()
 
